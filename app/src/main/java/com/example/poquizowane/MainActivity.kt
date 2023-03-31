4b200535ccdae66fa754a2673df22f25d1df24c2
SzymonKieczkaStudent <260483@student.pwr.edu.pl>
2023-03-31 15:16:13 +0200

    Splash screen initial version - files from figma moved to the same folder as main activity.

--- app/src/main/java/com/example/poquizowane/MainActivity.kt
+++ app/src/main/java/com/example/poquizowane/MainActivity.kt
@@ -1,22 +1,16 @@
 package com.example.poquizowane
 
 import android.os.Bundle
-import android.text.style.BackgroundColorSpan
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.poquizowane.appname.AppName
 
 import com.example.poquizowane.ui.theme.PoQuizowaneTheme
 
