bc0d6d4aab48c5ac86d38c16fb2306a604cd9037
SzymonKieczkaStudent <260483@student.pwr.edu.pl>
2023-03-31 14:47:58 +0200

    Splash screen initial version.

--- app/src/main/java/com/example/poquizowane/MainActivity.kt
+++ app/src/main/java/com/example/poquizowane/MainActivity.kt
@@ -1,30 +1,51 @@
 package com.example.poquizowane
 
 import android.os.Bundle
+import android.text.style.BackgroundColorSpan
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.poquizowane.appname.AppName
+
 import com.example.poquizowane.ui.theme.PoQuizowaneTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PoQuizowaneTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                Column (
+                    modifier = Modifier.fillMaxSize().background(Color(0, 151, 91))
                 ) {
-                    Greeting("PoQuizowane")
+                    AppName(modifier = Modifier.fillMaxSize(1.0f))
                 }
             }
+        }
+    }
+}
+
+
+@Preview(showBackground = true) //, backgroundColor = 4278228827)
+@Composable
+fun DefaultPreview() {
+    PoQuizowaneTheme {
+        Column (
+            modifier = Modifier.fillMaxSize().background(Color(0, 151, 91))
+            //color = MaterialTheme.colorScheme.contentColorFor(Color(0, 151, 91))
+        ) {
+            AppName(modifier = Modifier.fillMaxSize(1.0f))
         }
     }
 }
@@ -34,10 +55,3 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    PoQuizowaneTheme {
-        Greeting("Android")
-    }
-}
