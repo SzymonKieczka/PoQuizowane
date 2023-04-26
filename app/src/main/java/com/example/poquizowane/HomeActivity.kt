0a52ce15999ad559b142872578ecf4e606af5443
SzymonKieczkaStudent <260483@student.pwr.edu.pl>
2023-04-26 14:30:20 +0200

    temporary home screen - to be finished

--- app/src/main/java/com/example/poquizowane/HomeActivity.kt
+++ app/src/main/java/com/example/poquizowane/HomeActivity.kt
@@ -1,9 +1,12 @@
 package com.example.poquizowane
 
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
@@ -13,6 +16,9 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.poquizowane.ui.theme.PoQuizowaneTheme
 import com.google.firebase.auth.ktx.auth
@@ -23,16 +29,25 @@ class HomeActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PoQuizowaneTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    LogoutButton()
+                    BackgroundImage()
+                    //LogoutButton()
                 }
             }
         }
     }
+
+    @Composable
+    fun BackgroundImage() {
+        Image(painter = painterResource(id = R.drawable.homescreenpic),
+            contentDescription = "pic",
+            contentScale = ContentScale.FillBounds
+        )
+    }
+
     @Composable
     fun LogoutButton() {
         Column(
@@ -47,6 +62,21 @@ class HomeActivity : ComponentActivity() {
                 modifier = Modifier.padding(top = 16.dp)
             ) {
                 Text("Logout")
+            }
+        }
+    }
+
+    @Preview
+    @Composable
+    fun prev() {
+        PoQuizowaneTheme {
+            // A surface container using the 'background' color from the theme
+            Surface(
+                modifier = Modifier.fillMaxSize()
+                //color = MaterialTheme.colors.background
+            ) {
+                //LogoutButton()
+                BackgroundImage()
             }
         }
     }
